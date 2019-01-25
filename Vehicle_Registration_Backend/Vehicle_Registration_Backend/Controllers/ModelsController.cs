7ddc4843148f89ac7ef8bf72ac62993bf46f1b45
unknown <TerenceFrade@gmail.com>
2019-01-25 11:31:03 +0200

    Implemented Make and Model data-Objects, used entity framkework to make their Controllers. Used Mapper to map each object accordingly. Made a DbContext to add default SeedData in the Database for deployment

---
+++ Vehicle_Registration_Backend/Vehicle_Registration_Backend/Controllers/ModelsController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Vehicle_Registration_Backend.Models;
+
+namespace Vehicle_Registration_Backend.Controllers
+{
+    public class ModelsController : Controller
+    {
+        private readonly VehicleRegistrationContext _context;
+
+        public ModelsController(VehicleRegistrationContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Models
+        public async Task<IActionResult> Index()
+        {
+            var vehicleRegistrationContext = _context.Model.Include(m => m.Make);
+            return View(await vehicleRegistrationContext.ToListAsync());
+        }
+
+        // GET: Models/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var model = await _context.Model
+                .Include(m => m.Make)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (model == null)
+            {
+                return NotFound();
+            }
+
+            return View(model);
+        }
+
+        // GET: Models/Create
+        public IActionResult Create()
+        {
+            ViewData["MakeId"] = new SelectList(_context.Set<Make>(), "Id", "Id");
+            return View();
+        }
+
+        // POST: Models/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,MakeId")] Model model)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(model);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MakeId"] = new SelectList(_context.Set<Make>(), "Id", "Id", model.MakeId);
+            return View(model);
+        }
+
+        // GET: Models/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var model = await _context.Model.FindAsync(id);
+            if (model == null)
+            {
+                return NotFound();
+            }
+            ViewData["MakeId"] = new SelectList(_context.Set<Make>(), "Id", "Id", model.MakeId);
+            return View(model);
+        }
+
+        // POST: Models/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,MakeId")] Model model)
+        {
+            if (id != model.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(model);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ModelExists(model.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MakeId"] = new SelectList(_context.Set<Make>(), "Id", "Id", model.MakeId);
+            return View(model);
+        }
+
+        // GET: Models/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var model = await _context.Model
+                .Include(m => m.Make)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (model == null)
+            {
+                return NotFound();
+            }
+
+            return View(model);
+        }
+
+        // POST: Models/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var model = await _context.Model.FindAsync(id);
+            _context.Model.Remove(model);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ModelExists(int id)
+        {
+            return _context.Model.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Vehicle_Registration_Backend/Vehicle_Registration_Backend/Data/VehicleRegistrationContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Vehicle_Registration_Backend.Models;
+
+namespace Vehicle_Registration_Backend.Models
+{
+    public class VehicleRegistrationContext : DbContext
+    {
+        public VehicleRegistrationContext (DbContextOptions<VehicleRegistrationContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Vehicle_Registration_Backend.Models.Model> Model { get; set; }
+
+        public DbSet<Vehicle_Registration_Backend.Models.Make> Make { get; set; }
+    }
+}
