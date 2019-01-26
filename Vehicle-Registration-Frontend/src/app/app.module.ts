c0a28fe9d9eca7b22887e3cd723c66ad944daacb
unknown <TerenceFrade@gmail.com>
2019-01-26 18:06:26 +0200

    Passing statis JSON through compnent vehicle to vehicle item and iterating through it

--- Vehicle-Registration-Frontend/src/app/app.module.ts
+++ Vehicle-Registration-Frontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { VehiclesComponent } from './components/vehicles/vehicles.component';
+import { VehicleItemComponent } from './components/vehicle-item/vehicle-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    VehiclesComponent
+    VehiclesComponent,
+    VehicleItemComponent
   ],
   imports: [
     BrowserModule,
---
+++ Vehicle-Registration-Frontend/src/app/components/vehicle-item/vehicle-item.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit, Input } from "@angular/core";
+import { Vehicle } from "src/app/models/Vehicle";
+
+@Component({
+  selector: "app-vehicle-item",
+  templateUrl: "./vehicle-item.component.html",
+  styleUrls: ["./vehicle-item.component.css"]
+})
+export class VehicleItemComponent implements OnInit {
+  @Input() vehicle: Vehicle;
+  constructor() {}
+
+  ngOnInit() {}
+}
